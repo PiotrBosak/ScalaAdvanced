44a23b1aada8dd00c172087c1a9dfc68fe7245ae
piotr <PiotrBosak@yahoo.com>
2020-05-05 13:24:31 +0200

    finished set

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -13,7 +13,10 @@ trait MySet[A] extends (A => Boolean){
   def flatMap[B](f: A => MySet[B]): MySet[B]
   def filter(predicate : A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
-
+  def -(elem:A): MySet[A]
+  def &(set:MySet[A]): MySet[A]
+  def --(set:MySet[A]): MySet[A]
+  def unary_! : MySet[A]
   override def apply(v1: A): Boolean = contains(v1)
 
 
@@ -33,7 +36,51 @@ class EmptySet[A] extends MySet[A]{
 
   override def foreach(f: A => Unit): Unit = ()
 
+  override def -(elem: A): MySet[A] = this
+
+  override def &(set: MySet[A]): MySet[A] = this
+
+  override def --(set: MySet[A]): MySet[A] = this
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
+
+
 }
+
+// all elements of type A which satisfy a property
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  override def contains(elem: A): Boolean = property(elem)
+
+  override def +(elem: A): MySet[A] =
+     new PropertyBasedSet[A](x => property(x) || x == elem)
+
+  override def ++(anotherSet: MySet[A]): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || anotherSet.contains(x))
+
+  override def map[B](f: A => B): MySet[B] = throwExc
+
+
+  def throwExc = throw new IllegalArgumentException
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = throwExc
+
+  override def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+
+  override def foreach(f: A => Unit): Unit = throwExc
+
+  override def -(elem: A): MySet[A] = filter(x => x != elem)
+
+  override def &(set: MySet[A]): MySet[A] = filter(set)
+
+  override def --(set: MySet[A]): MySet[A] = filter(!set)
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+}
+/*
+- remove element
+-intersection with another set
+-difference with another set
+ */
 
 class NonEmptySet[A](val head: A, val  tail: MySet[A]) extends MySet[A] {
   override def contains(elem: A): Boolean =
@@ -61,6 +108,26 @@ class NonEmptySet[A](val head: A, val  tail: MySet[A]) extends MySet[A] {
     else filteredTail
   }
 
+  override def -(elem: A): MySet[A] =
+    if(head == elem) tail
+    else tail - elem + head
+
+  override def --(set: MySet[A]): MySet[A] = {
+//   if(set contains head) tail
+//   else tail -- set + head
+    filter(x => !set(x))
+  }
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
+
+
+
+  override def &(set: MySet[A]): MySet[A] = {
+//    if(set contains head) tail & set + head
+//    else tail & set doesn't work
+    filter(set)// filter( x => set.contains(x))
+  }
+
   override def foreach(f: A => Unit): Unit = {
     f(head)
     tail.foreach(f)
@@ -72,11 +139,22 @@ class NonEmptySet[A](val head: A, val  tail: MySet[A]) extends MySet[A] {
 }
 object MySet{
   def apply[A](values: A*): MySet[A] = {
-    @scala.annotation.tailrec
     def buildSet(valSeq: Seq[A], acc: MySet[A]): MySet[A] =
-      if(valSeq.isEmpty) acc
-      else buildSet(valSeq.tail,acc+valSeq.head)
+      null
+
     buildSet(values.toSeq,new EmptySet[A])
   }
+
+}
+object myApp extends App {
+  val set = new NonEmptySet[Int](1, new NonEmptySet[Int](2,new NonEmptySet[Int](3,new EmptySet[Int])))
+  val set2 = new NonEmptySet[Int](2, new NonEmptySet[Int](4,new NonEmptySet[Int](5,new EmptySet[Int])))
+  val intersect = set & set2
+  val difference = set -- set2
+  println(intersect.contains(2))
+  println(intersect.contains(3))
+  println("-----------")
+  //println(difference.contains(2))
+  //println(difference.contains(3))
 
 }
--- src/playground/JavaPlayground.java
+++ src/playground/JavaPlayground.java
@@ -1,12 +1,22 @@
 package playground;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JavaPlayground {
     public static void main(String... args) {
         System.out.println("java works");
+        List<Integer> list = List.of(1,3,5,6);
+        list = list.stream().flatMap(x -> List.of(x,x+1).stream()).collect(Collectors.toList());
+        for(Integer i: list)
+            System.out.println(i);
         System.out.println(fun(1,2,3,5,5,4));
+
     }
 
     static int fun(int... integers){
         return integers.length;
     }
+
 }
