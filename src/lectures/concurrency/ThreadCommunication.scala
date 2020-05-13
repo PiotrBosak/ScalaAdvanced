e9f5a5c8356be405246f09c5d933a9adbd492d92
piotr <PiotrBosak@yahoo.com>
2020-05-13 11:38:23 +0200

    finished consumer-producer

--- src/lectures/concurrency/ThreadCommunication.scala
+++ src/lectures/concurrency/ThreadCommunication.scala
@@ -1,31 +1,39 @@
 package lectures.concurrency
 
-object ThreadCommunication extends App{
+import scala.collection.mutable
+import scala.util.Random
+
+object ThreadCommunication extends App {
+
   /*
   the producer-consumer problem
 
    */
   class SimpleContainer {
     private var value: Int = 0
-    def isEmpty: Boolean =  value == 0
+
+    def isEmpty: Boolean = value == 0
+
     def getValue: Int = {
       val result = value
       value = 0
       result
     }
+
     def setValue(v: Int): Unit = value = v
   }
+
   def naiveProducers(): Unit = {
     val container = new SimpleContainer
 
     val consumer = new Thread(() => {
       println("consumer is waiting")
-      while(container.isEmpty)//busy waiting
+      while (container.isEmpty) //busy waiting
         println("still waiting")
       println("I've consumed " + container.getValue)
     })
 
-    val  producer = new Thread(() => {
+    val producer = new Thread(() => {
       println("I haven't produced yet")
       Thread.sleep(500)
       val value = 5
@@ -36,7 +44,8 @@ object ThreadCommunication extends App{
     producer.start()
 
   }
-//  naiveProducers()
+
+  //  naiveProducers()
 
   //wait and notify
   def smartProdCons() = {
@@ -72,8 +81,122 @@ object ThreadCommunication extends App{
     producer.start()
 
   }
-  smartProdCons()
+
+  //  smartProdCons()
+
+  /*
+  producer produces in 1 of 3 values
+  consumer consumer new value
+
+   */
+  def bufferProdCons(): Unit = {
+    val buffer = new mutable.Queue[Int]
+    val capacity = 3
+    val consumer = new Thread(() => {
+      val random = new Random(System.nanoTime())
+      while (true) {
+        buffer.synchronized {
+          if (buffer.isEmpty) {
+            buffer.wait()
+            println("consumer waiting")
+          }
+
+          val value = buffer.dequeue()
+          println(s"I've got $value ")
+          //just in case
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(250))
+      }
+    })
+
+    val producer = new Thread(() => {
+      val random = new Random(System.nanoTime())
+      var i: Int = 0
+      while (true) {
+        buffer.synchronized {
+          if (buffer.size == capacity) {
+            buffer.wait()
+            println("producer waiting")
+          }
+          buffer.enqueue(i)
+          println("produced " + i)
+          buffer.notify()
+          i += 1
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    })
+    producer.start()
+    consumer.start()
 
 
+  }
+
+  //bufferProdCons()
+
+  /*
+  multiple consumers, producers
+  acting on the same buffer
+
+   */
+  class ConsumerRunnable(id: Int, val buffer: mutable.Queue[Int]) extends Runnable {
+    override def run(): Unit = {
+      while (true) {
+        buffer.synchronized {
+          while (buffer.isEmpty) {
+            println(s"consumer$id is waiting")
+            buffer.wait()
+          }
+            val x = buffer.dequeue()
+            println(s"I've got $x")
+            buffer.notifyAll()
+
+        }
+        Thread.sleep(ConsumerRunnable.random.nextInt(1000))
+      }
+
+    }
+  }
+
+  object ConsumerRunnable {
+    val random = new Random()
+
+  }
+
+  class ProducerRunnable(id: Int, val buffer: mutable.Queue[Int]) extends Runnable {
+    override def run(): Unit = {
+      var i = 0
+      while (true) {
+        buffer.synchronized {
+          while (buffer.size >= ProducerRunnable.capacity) {
+            println(s"producer$id is waiting")
+            buffer.wait()
+          }
+
+            buffer.enqueue(i)
+            buffer.notifyAll()
+            i += 1
+
+        }
+        Thread.sleep(ProducerRunnable.random.nextInt(1000))
+      }
+    }
+  }
+
+  object ProducerRunnable {
+    val capacity = 5
+    val random = new Random()
+  }
+
+  def multipleProdCons(): Unit = {
+    val buffer = new mutable.Queue[Int]
+    for (i <- 1 to 3 )
+      new Thread(new ConsumerRunnable(i, buffer)).start()
+    for (i <- 1 to 3)
+      new Thread(new ProducerRunnable(i, buffer)).start()
+  }
+
+  multipleProdCons()
 
 }
